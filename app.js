cc0f67c9b99a95daf8d298988796ad3133497692
LeFLEUR-ui <marvinfabricante630@gmail.com>
2025-08-26 18:57:53 +0800

    Update app.js

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ window.addEventListener('scroll', () => {
     lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
 });
 
-// document.addEventListener('contextmenu', event => event.preventDefault());
-// document.addEventListener('keydown', function(e) {
-//     if (e.key === "F12" || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) || (e.ctrlKey && e.key === 'U')) {
-//         e.preventDefault();
-//     }
-// });
+document.addEventListener('contextmenu', event => event.preventDefault());
+document.addEventListener('keydown', function(e) {
+    if (e.key === "F12" || (e.ctrlKey && e.shiftKey && (e.key === 'I' || e.key === 'J')) || (e.ctrlKey && e.key === 'U')) {
+        e.preventDefault();
+    }
+});
